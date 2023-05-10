dfccd1944bc8b0a1a15dd1a5997f1fe7f47f6fe6
Ethan Headings <eheadin@us.ibm.com>
2023-05-09 22:36:56 -0400

    add header v1

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,10 +8,47 @@ export const handler: Handlers<Post[]> = {
   },
 };
 
+type Props = {
+  active: string;
+};
+
+function Header({ active }: Props) {
+  const menus = [
+    { name: "Home", href: "/" },
+    { name: "Components", href: "/components" },
+    { name: "Docs", href: "/docs" },
+  ];
+
+  return (
+    <div class="bg-white w-full max-w-screen-lg py-6 px-8 flex flex-col md:flex-row gap-4">
+      <div class="flex items-center flex-1">
+        <LemonIcon />
+        <div class="text-2xl  ml-1 font-bold">
+          Fresh
+        </div>
+      </div>
+      <ul class="flex items-center gap-6">
+        {menus.map((menu) => (
+          <li>
+            <a
+              href={menu.href}
+              class={"text-gray-500 hover:text-gray-700 py-1 border-gray-500" +
+                (menu.href === active ? " font-bold border-b-2" : "")}
+            >
+              {menu.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function BlogIndexPage(props: PageProps<Post[]>) {
   const posts = props.data;
   return (
     <main class="max-w-screen-md px-4 pt-16 mx-auto">
+      <Header/>
       <h1 class="text-5xl font-bold">Blog</h1>
       <div class="mt-8">
         {posts.map((post) => <PostCard post={post} />)}
